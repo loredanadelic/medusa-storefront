f1c33871735544a413dff93f09bc7e78dec56602
loredanadelic <loredana.delic@gmail.com>
2023-06-25 18:57:36 +0200

    initital commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { MedusaProvider } from "medusa-react";
+import { QueryClient } from "@tanstack/react-query";
+import React from "react";
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MedusaProvider
+      queryClientProviderProps={{ client: queryClient }}
+      baseUrl="http://localhost:9000"
+    >
+      <Component {...pageProps} />
+    </MedusaProvider>
+  );
 }
