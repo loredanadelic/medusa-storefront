e3663ca473f77f9f1cc142435e1ea80e707d414e
loredanadelic <loredana.delic@gmail.com>
2023-06-27 09:28:38 +0200

    Added logo

--- modules/layout/components/medusa/index.tsx
+++ modules/layout/components/medusa/index.tsx
@@ -1,7 +1,10 @@
 const Medusa = () => {
   return (
-    <div>
-      <h1 className=" text-2xl text-center">medusa</h1>
+    <div className=" w-[100%] flex justify-center">
+        <div className="w-4 mr-1 mt-3  ">
+        <img src='/logo.png'></img></div>
+        <div className="ml-1">
+      <h1 className=" text-2xl text-center ">medusa</h1></div>
     </div>
   );
 };
