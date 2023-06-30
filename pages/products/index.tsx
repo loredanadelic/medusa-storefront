c78cddae5c4f37578ab726d26e0c3aacb0ace1c9
loredanadelic <loredana.delic@gmail.com>
2023-06-30 08:33:54 +0200

    changed the way of data fetching on products page

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,27 +1,61 @@
 import { PricedProduct } from "@medusajs/medusa/dist/types/pricing";
-import { useEffect, useState } from "react";
 import { medusaClient } from "@/lib/config";
 import Product from "@/modules/products/components/product";
+import { GetStaticProps } from "next";
+import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
+
+const fetchProducts = async () => {
+  return await medusaClient.products.list().then(({ products }) => products);
+};
 
 const ProductsPage = () => {
-  const [productList, setProductList] = useState<PricedProduct[]>([]);
-  const fetchProduct = async () => {
-    return await medusaClient.products.list().then(({ products }) => {
-      setProductList(products);
-    });
-  };
-
-  useEffect(() => {
-    fetchProduct();
-  }, []);
-
-  return (
+  const { data, isError, isLoading, isSuccess } = useQuery(
+    [`get_product`],
+    () => fetchProducts(),
+    {}
+  );
+  if (isError) {
+    return (
+      <div>
+        <p>Something went wrong</p>
+      </div>
+    );
+  }
+  if (isLoading || !data) {
+    return (
+      <div>
+        <p>Loading</p>
+      </div>
+    );
+  }
+  if (isSuccess) {
+    return (
       <ul className="grid gap-3 grid-cols-4">
-        {productList.map((product: PricedProduct) => {
+        {data.map((product: PricedProduct) => {
           return <Product product={product} key={product.id} />;
         })}
       </ul>
-  );
+    );
+  }
 };
 
+export const getStaticProps: GetStaticProps = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery([`get_products`], () => fetchProducts());
+  const queryData = await queryClient.getQueryData([`get_products`]);
+  if (!queryData) {
+    return {
+      props: {
+        notFound: true,
+      },
+    };
+  }
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      notFound: false,
+      revalidate: 60,
+    },
+  };
+};
 export default ProductsPage;
