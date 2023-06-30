eb7e67174db50eff8296e9ad7ee9939f0c60b60c
loredanadelic <loredana.delic@gmail.com>
2023-06-30 08:41:35 +0200

    linking pages

--- modules/products/components/product/index.tsx
+++ modules/products/components/product/index.tsx
@@ -1,21 +1,26 @@
 import { PricedProduct } from "@medusajs/medusa/dist/types/pricing";
 import Thumbnail from "../thumbnail";
 import { getPrice } from "@/lib/util/product";
+import { redirect } from "next/dist/server/api-utils";
+import Link from "next/link";
 type ProductProps = {
   product: PricedProduct;
 };
 const Product: React.FC<ProductProps> = ({ product }) => {
-  const {price, currency}= getPrice(product)
+  const { price, currency } = getPrice(product);
+ 
   return (
-    <li className="h-auto w-auto relative hover:cursor-pointer">
-      <Thumbnail thumbnail={product.thumbnail || ""} />
-      <div className="flex justify-between absolute bottom-0 px-4 py-3 w-full ">
-        <p className="m-0.5 text-lg">{product.title}</p>
-        <p className="m-0.5 text-lg">
-          {price} {currency.toUpperCase()}
-        </p>
-      </div>
-    </li>
+    <Link href={`/products/${product.id}`}>
+      <li className="h-auto w-auto relative hover:cursor-pointer">
+        <Thumbnail thumbnail={product.thumbnail || ""} />
+        <div className="flex justify-between absolute bottom-0 px-4 py-3 w-full ">
+          <p className="m-0.5 text-lg">{product.title}</p>
+          <p className="m-0.5 text-lg">
+            {price} {currency.toUpperCase()}
+          </p>
+        </div>
+      </li>
+    </Link>
   );
 };
 
