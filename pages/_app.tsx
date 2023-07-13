0f1e8b44a87f62ac6ee1a5d84836899639bf8ea0
loredanadelic <loredana.delic@gmail.com>
2023-07-13 10:58:01 +0200

    Removed medusa-react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { MedusaProvider } from "medusa-react";
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from '@tanstack/react-query'
 import React from "react";
-import { API_URL, queryClient } from "@/lib/config";
+import {  queryClient } from "@/lib/config";
 import Layout from "@/modules/layout/templates";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MedusaProvider
-      queryClientProviderProps={{ client: queryClient }}
-      baseUrl={API_URL}
-    >
+  <QueryClientProvider client={queryClient}>
       <Layout>
         <div className=" min-h-screen items-center justify-between p-4 bg-white">
           <Component {...pageProps} />
         </div>
       </Layout>
-    </MedusaProvider>
+  </QueryClientProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import Layout from "@/modules/layout/templates";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
